416b7b599e5579f6cec87064ba9429eb3172fe55
khall717 <khall@restaurant.com>
2023-10-29 11:59:31 -0500

    Add additional tests

---
+++ MarsRoverTest.Tests/GridTests.cs
@@ -0,0 +1,44 @@
+﻿using MarsRover.Models;
+using MarsRover.Utility;
+using NuGet.Frameworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarsRover.Models;
+using MarsRover.Utility;
+using MarsRoverTest.Tests;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+namespace MarsRoverTest.Tests
+{
+    [TestClass]
+    public class GridTests
+    {
+        [TestMethod]
+        public void TestGridCreation()
+        {
+            var classToReadFile = new SanitizeFile { fileName = "../../../testData.txt" };
+            string[] fileContents = new string[0];
+            fileContents = classToReadFile.SanitizeFileContents(classToReadFile.fileName);
+
+            
+            //arrange
+            string grabFirstLineInstructions = fileContents[0];
+            Regex regex = new Regex(@"\d+"); //verify first line contains numbers
+            MatchCollection matches = regex.Matches(grabFirstLineInstructions);
+            if (matches.Count != 2)
+            {
+                Console.WriteLine("Hm, please check grid boundaries provided and try again.");
+                Environment.Exit(0);
+            }
+            (string X, string Y) = (matches[0].Value, matches[1].Value); //Set X and Y coordinates for grid
+            Grid grid = new Grid(int.Parse(X), int.Parse(Y));
+            Assert.AreEqual(5, grid.latitude);
+            Assert.AreEqual(5, grid.longitude);
+        }
+    }
+    
+}
